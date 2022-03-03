789462362f05f94387598744f1e089b7f45159c2
MicaelRiboura <micaelribouradossantos@gmail.com>
2022-03-03 07:42:12 -0300

    feat(GamesRepository): add query of findUsersByGameId

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -24,8 +24,10 @@ export class GamesRepository implements IGamesRepository {
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
-    return this.repository
+    return await this.repository
       .createQueryBuilder()
-      // Complete usando query builder
+      .relation(Game, "users")
+      .of(id)
+      .loadMany();
   }
 }
